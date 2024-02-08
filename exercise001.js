27125bd57fbc4d4d07869a18df78f7d3d47d18d9
Helen Pickavance <ellohez@gmail.com>
2024-02-08 16:40:05 +0000

    fix: refactored to use an arrow function and fixed let variable declaration

--- exercise001.js
+++ exercise001.js
@@ -17,9 +17,9 @@ const promise = new Promise((resolve, reject) => {
 // 	.catch(error => console.log(`Boo. Promise rejected with response: ${error}`));
 
 // Task 2 async, await - try{} catch{}
-async function callPromise() {
+const callPromise = async () => {
 	try{
-		let result = await promise;
+		const result = await promise;
 		console.log(`Yay! Promise resolved with response ${result}`);
 	} catch(error) {
 		console.error(`Boo. Promise rejected with response: ${error}`);
